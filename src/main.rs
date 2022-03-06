6a4bdef6f7d8584ae2ff6e426cde77b0b85e90a8
Hidetake Iwata <int128@gmail.com>
2022-03-06 16:43:01 +0900

    Show instances

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let shared_config = aws_config::load_from_env().await;
+    let client = aws_sdk_ec2::Client::new(&shared_config);
+
+    let describe_instances = client.
+        describe_instances().
+        send().
+        await.
+        unwrap();
+
+    for r in describe_instances.reservations.unwrap_or_default() {
+        for i in r.instances.unwrap_or_default() {
+            let instance_id = i.instance_id.unwrap();
+            println!("instance_id={}", instance_id)
+        }
+    }
 }
