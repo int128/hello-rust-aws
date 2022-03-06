b5ddbca10721b43f907c1654571946a3d17bf455
Hidetake Iwata <int128@gmail.com>
2022-03-06 17:18:01 +0900

    refactor: extract fn

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), aws_sdk_ec2::Error> {
     let shared_config = aws_config::load_from_env().await;
     let client = aws_sdk_ec2::Client::new(&shared_config);
+    show_instances(&client).await
+}
 
+async fn show_instances(client: &aws_sdk_ec2::Client) -> Result<(), aws_sdk_ec2::Error> {
     let describe_instances = client.
         describe_instances().
         send().
-        await.
-        unwrap();
+        await?;
 
     for r in describe_instances.reservations.unwrap_or_default() {
         for i in r.instances.unwrap_or_default() {
             let instance_id = i.instance_id.unwrap();
-            println!("instance_id={}", instance_id)
+            println!("instance_id={}", instance_id);
         }
     }
+
+    Ok(())
 }
